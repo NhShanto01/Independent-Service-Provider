d88c0b2ef553600cb2bc2a8f3da622d063a61ff8
NhShanto01 <mdnurhossain940@gmail.com>
2022-04-17 00:37:03 +0600

    Home Page Complete

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Header from './Pages/Shared/Header/Header';
 import Footer from './Pages/Shared/Footer/Footer';
+import Home from './Pages/Home/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Routes>
-        <Route></Route>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import CustomLink from '../../CustomLink/CustomLink';
 import './Header.css'
 
 const Header = () => {
-    return (
-        <div className='header'>
-            <h2 >welcome to my new website</h2>
-        </div>
-    );
+  return (
+    <div className='header'>
+      <Navbar bg="light" expand="lg" >
+        <Container>
+          <Navbar.Brand href="#home" className='text-success fw-bold'>Edu-Phi</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link as={CustomLink} to="home">Home</Nav.Link>
+              <Nav.Link as={CustomLink} to="services">Services</Nav.Link>
+              <Nav.Link as={CustomLink} to="blogs">Blogs</Nav.Link>
+              <Nav.Link as={CustomLink} to="about">About Me</Nav.Link>
+              <Nav.Link as={CustomLink} to="contact">Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
 };
 
 export default Header;
