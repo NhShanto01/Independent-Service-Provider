4fca8a0be60cefcc9224be436fc6e49e416375d3
NhShanto01 <mdnurhossain940@gmail.com>
2022-04-16 22:40:19 +0600

    added some components and some packages install

---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <h2 className='text-danger'>this is footer</h2>
+            Andrew Phillips
+        </div>
+    );
+};
+
+export default Footer;
