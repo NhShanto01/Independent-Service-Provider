229a0ad1f41c00a5945b0fc1b964077f77fdf250
NhShanto01 <mdnurhossain940@gmail.com>
2022-04-17 05:11:26 +0600

    added checkout page with from

--- src/Pages/Home/CheckOut/CheckOut.js
+++ src/Pages/Home/CheckOut/CheckOut.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import { Button, FloatingLabel, Form } from 'react-bootstrap';
 
 const CheckOut = () => {
     return (
-        <div>
-            this is CheckOut
+        <div className='container w-50'>
+            <h1>This is checkout</h1>
+            <Form className=''>
+
+                <Form.Group className='mb-3' controlId="formGridEmail">
+
+                    <Form.Control type="email" placeholder="Your Name" />
+                </Form.Group>
+                <Form.Group className='mb-3' controlId="formGridEmail">
+
+                    <Form.Control type="email" placeholder="Your email" />
+                </Form.Group>
+
+                <Form.Group className='mb-3' controlId="formGridPassword">
+
+                    <Form.Control type="phone" placeholder="Your Phone" />
+                </Form.Group>
+
+
+                <FloatingLabel className='mb-3' controlId="floatingTextarea2" label="Address">
+                    <Form.Control
+                        as="textarea"
+                        placeholder="Leave a comment here"
+                        style={{ height: '100px' }}
+                    />
+                </FloatingLabel>
+
+                <Form.Group className="mb-3 text-start" id="formGridCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+
+                <Button variant="success" type="submit">
+                    Submit
+                </Button>
+            </Form>
         </div>
     );
 };
--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './Service.css';
 
 const Service = ({ service }) => {
@@ -10,13 +10,16 @@ const Service = ({ service }) => {
     }
     return (
         <div className='g-2 col-sm-12 col-md-6 col-lg-4 '>
-            <div className="card shadow p-3 mb-5 bg-body rounded" style={{ width: "18rem" }}>
-                <img src={picture} className="card-img-top" alt="..." />
+            <div className="card shadow p-3 mb-5 bg-body rounded" style={{ width: "20rem" }}>
+                <img src={picture} className="card-img-top img-fluid" alt="..." />
                 <div className='card-body'>
                     <h5 className="card-title">{name}</h5>
-                    <p>Price:${balance}</p>
+                    <p>Price:{balance}</p>
                     <p className="card-text">{description}</p>
-                    <a href="#"><button onClick={() => navigateToServiceDetail(id)} className='btn btn-success text-white'>CheckOut:{name}</button></a>
+                    {/* <a href="#"></a> */}
+                    <Link to="/checkout">
+                    <button onClick={() => navigateToServiceDetail(id)} className='btn btn-success text-white'>Buy : {name} pack</button>
+                </Link>
                 </div>
             </div>
         </div>
